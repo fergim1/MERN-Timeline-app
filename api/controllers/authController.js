bab4588395c2214374b4afb47aa57a226e323afd
fergim1 <fernando@movenow.com.ar>
2021-05-10 20:26:58 -0300

    Crear usuario con password encriptado y JWT - LISTO . Login tambien - LISTO

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,6 +1,7 @@
 const { response } = require('express')
 const bcrypt = require('bcryptjs');
 const Usuario = require('../models/Usuario')
+const { generarJWT } = require('../helpers/jwt');
 
 const crearUsuario = async (req, res=response) =>{
 
@@ -23,11 +24,15 @@ const crearUsuario = async (req, res=response) =>{
         usuario.password = bcrypt.hashSync( password, salt );
         
         await usuario.save()
+
+    // Generar JWT
+        const token = await generarJWT( usuario.id, usuario.name );
     
         res.status(201).json({
             ok: true,
             uid: usuario.id,
-            name: usuario.name
+            name: usuario.name,
+            token
         })
         
     } catch (error) {
@@ -63,10 +68,14 @@ const loginUsuario = async ( req, res=response ) =>{
         });
     }
 
+// Generar JWT
+    const token = await generarJWT( usuario.id, usuario.name );
+
     res.status(201).json({
         ok: true,
         uid: usuario.id,
-        name: usuario.name
+        name: usuario.name,
+        token
     })
     
 }
